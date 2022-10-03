54f0ec77d8491987fab5a673bf0b41c307f588b5
Kai Schmidt <electronickai@gmail.com>
2022-10-03 19:51:14 +0200

    convert byte code test exemple to just check methods instead of code units

--- lib/myArchTestLib/src/main/java/hamburg/kaischmidt/myarctestlib/arctests/AddMethodsMustAccessSetCondition.java
+++ lib/myArchTestLib/src/main/java/hamburg/kaischmidt/myarctestlib/arctests/AddMethodsMustAccessSetCondition.java
@@ -1,6 +1,6 @@
 package hamburg.kaischmidt.myarctestlib.arctests;
 
-import com.tngtech.archunit.core.domain.JavaCodeUnit;
+import com.tngtech.archunit.core.domain.JavaMethod;
 import com.tngtech.archunit.core.domain.JavaFieldAccess;
 import com.tngtech.archunit.lang.ArchCondition;
 import com.tngtech.archunit.lang.ConditionEvents;
@@ -8,7 +8,7 @@ import com.tngtech.archunit.lang.SimpleConditionEvent;
 
 import java.util.Set;
 
-public class AddMethodsMustAccessSetCondition extends ArchCondition<JavaCodeUnit> {
+public class AddMethodsMustAccessSetCondition extends ArchCondition<JavaMethod> {
 
     public AddMethodsMustAccessSetCondition() {
         this("access fields of type Set if the method starts with add");
@@ -19,19 +19,19 @@ public class AddMethodsMustAccessSetCondition extends ArchCondition<JavaCodeUnit
     }
 
     @Override
-    public void check(JavaCodeUnit codeUnit, ConditionEvents events) {
-        if (!codeUnit.getName().startsWith("add")) {
+    public void check(JavaMethod method, ConditionEvents events) {
+        if (!method.getName().startsWith("add")) {
             return;
         }
 
-        for (JavaFieldAccess fieldAccess : codeUnit.getFieldAccesses()) {
+        for (JavaFieldAccess fieldAccess : method.getFieldAccesses()) {
             if (fieldAccess.getTarget().getType().toErasure().isAssignableFrom(Set.class)) {
-                events.add(SimpleConditionEvent.satisfied(codeUnit, "CodeUnit "
-                        + codeUnit + " starting with add does access a field of type Set. All fine "));
+                events.add(SimpleConditionEvent.satisfied(method, "Method "
+                        + method + " starting with add does access a field of type Set. All fine "));
                 return;
             }
         }
-        events.add(SimpleConditionEvent.violated(codeUnit, "CodeUnit "
-                + codeUnit + " starting with add does not access a field of type Set "));
+        events.add(SimpleConditionEvent.violated(method, "Method "
+                + method + " starting with add does not access a field of type Set "));
     }
 }
--- testprj/core-example/src/test/java/de/test/core/AddMethodsMustAccessSetTest.java
+++ testprj/core-example/src/test/java/de/test/core/AddMethodsMustAccessSetTest.java
@@ -5,12 +5,12 @@ import com.tngtech.archunit.junit.ArchTest;
 import com.tngtech.archunit.lang.ArchRule;
 import hamburg.kaischmidt.myarctestlib.arctests.AddMethodsMustAccessSetCondition;
 
-import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.codeUnits;
+import static com.tngtech.archunit.lang.syntax.ArchRuleDefinition.methods;
 
 @AnalyzeClasses(packages = "de.test.core..")
 public class AddMethodsMustAccessSetTest {
 
   @ArchTest
-  static final ArchRule addMethodsMustAccessSet = codeUnits().should(new AddMethodsMustAccessSetCondition());
+  static final ArchRule addMethodsMustAccessSet = methods().should(new AddMethodsMustAccessSetCondition());
 
 }
